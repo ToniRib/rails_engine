1fa21998ebe9941e92e6b76eb535077d43c45222
ToniRib <tonimarierib@gmail.com>
2016-02-22 17:57:57 -0700

    Add endpoint for items#show

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -22,4 +22,25 @@ RSpec.describe Api::V1::ItemsController, type: :controller do
       expect(json_response.last["id"]).to eq(items.last.id)
     end
   end
+
+  describe "GET #show.json" do
+    let(:json_response) { JSON.parse(response.body) }
+    let(:item) { create(:item) }
+
+    it "responds with successful 200 HTTP status code" do
+      get :show, id: item.id, format: :json
+
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns the specific item in JSON format" do
+      get :show, id: item.id, format: :json
+
+      expect(json_response["id"]).to eq(item.id)
+      expect(json_response["description"]).to eq(item.description)
+      expect(json_response["unit_price"]).to eq(item.unit_price)
+      expect(json_response["merchant_id"]).to eq(item.merchant_id)
+    end
+  end
 end
