520e21453103a9a21ffac3f9aa1f525bc677ddf0
ToniRib <tonimarierib@gmail.com>
2016-02-22 17:48:15 -0700

    Add endpoint for customers#show

--- spec/controllers/customers_controller_spec.rb
+++ spec/controllers/customers_controller_spec.rb
@@ -21,4 +21,23 @@ RSpec.describe Api::V1::CustomersController, type: :controller do
       expect(json_response.last["id"]).to eq(customers.last.id)
     end
   end
+
+  describe "GET #show.json" do
+    let(:json_response) { JSON.parse(response.body) }
+    let(:customer) { create(:customer) }
+
+    it "responds with successful 200 HTTP status code" do
+      get :show, id: customer.id, format: :json
+
+      expect(response).to be_success
+    end
+
+    it "returns all of the customers in JSON format" do
+      get :show, id: customer.id, format: :json
+
+      expect(json_response["id"]).to eq(customer.id)
+      expect(json_response["first_name"]).to eq(customer.first_name)
+      expect(json_response["last_name"]).to eq(customer.last_name)
+    end
+  end
 end
