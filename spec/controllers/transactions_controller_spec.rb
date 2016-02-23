ef1dfaacd8789f968a45379fd03cd06dea7c5ccd
ToniRib <tonimarierib@gmail.com>
2016-02-22 17:59:32 -0700

    Add endpoint for transactions#show

--- spec/controllers/transactions_controller_spec.rb
+++ spec/controllers/transactions_controller_spec.rb
@@ -22,4 +22,25 @@ RSpec.describe Api::V1::TransactionsController, type: :controller do
       expect(json_response.last["invoice_id"]).to eq(transactions.last.invoice_id)
     end
   end
+
+  describe "GET #show.json" do
+    let(:json_response) { JSON.parse(response.body) }
+    let(:transaction) { create(:transaction) }
+
+    it "responds with successful 200 HTTP status code" do
+      get :show, id: transaction.id, format: :json
+
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "returns the specific transaction in JSON format" do
+      get :show, id: transaction.id, format: :json
+
+      expect(json_response["id"]).to eq(transaction.id)
+      expect(json_response["invoice_id"]).to eq(transaction.invoice_id)
+      expect(json_response["credit_card_number"]).to eq(transaction.credit_card_number)
+      expect(json_response["result"]).to eq(transaction.result)
+    end
+  end
 end
