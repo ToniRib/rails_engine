7249741f5c96f9009391ca611fd6610ce9366e69
ToniRib <tonimarierib@gmail.com>
2016-02-22 20:17:39 -0700

    Finish out merchants controller for find one

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -15,7 +15,11 @@ class Api::V1::MerchantsController < ApplicationController
     if params[:id]
       Merchant.find(params[:id])
     elsif params[:name]
-      Merchant.where("name ILIKE ?", params[:name]).first
+      Merchant.find_by("name ILIKE ?", params[:name])
+    elsif params[:created_at]
+      Merchant.find_by("created_at ILIKE ?", params[:created_at])
+    elsif params[:updated_at]
+      Merchant.find_by("updated_at ILIKE ?", params[:updated_at])
     end
   end
 end
--- spec/controllers/merchants_controller_spec.rb
+++ spec/controllers/merchants_controller_spec.rb
@@ -46,13 +46,6 @@ RSpec.describe Api::V1::MerchantsController, type: :controller do
     let(:json_response) { JSON.parse(response.body) }
     let(:merchant) { create(:merchant) }
 
-    it "responds with successful 200 HTTP status code" do
-      get :show, name: merchant.name, format: :json
-
-      expect(response).to be_success
-      expect(response).to have_http_status(200)
-    end
-
     it "returns the specific merchant in JSON format" do
       get :show, name: merchant.name, format: :json
 
@@ -62,6 +55,30 @@ RSpec.describe Api::V1::MerchantsController, type: :controller do
 
     it "returns the specific merchant in JSON format - case insensitive" do
       get :show, name: merchant.name.downcase, format: :json
+
+      expect(json_response["id"]).to eq(merchant.id)
+      expect(json_response["name"]).to eq(merchant.name)
+    end
+  end
+
+  describe "GET #show.json with created_at" do
+    let(:json_response) { JSON.parse(response.body) }
+    let(:merchant) { create(:merchant) }
+
+    it "returns the specific merchant in JSON format" do
+      get :show, created_at: merchant.created_at, format: :json
+
+      expect(json_response["id"]).to eq(merchant.id)
+      expect(json_response["name"]).to eq(merchant.name)
+    end
+  end
+
+  describe "GET #show.json with updated_at" do
+    let(:json_response) { JSON.parse(response.body) }
+    let(:merchant) { create(:merchant) }
+
+    it "returns the specific merchant in JSON format" do
+      get :show, updated_at: merchant.updated_at, format: :json
 
       expect(json_response["id"]).to eq(merchant.id)
       expect(json_response["name"]).to eq(merchant.name)
