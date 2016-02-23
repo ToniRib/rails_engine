f3358c3e549d09bafb6c57e7e87871970c5d57d4
ToniRib <tonimarierib@gmail.com>
2016-02-23 13:44:14 -0700

    Update routes/clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,39 @@
 Rails.application.routes.draw do
-  namespace :api do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get '/merchants/find_all', to: "merchants#index", defaults: { format: :json }
-      get '/merchants/find', to: "merchants#show", defaults: { format: :json }
-      get '/merchants/random', to: "random_merchant#show", defaults: { format: :json }
-      resources :merchants, only: [:index, :show], defaults: { format: :json }
+      get '/merchants/random', to: "random_merchant#show"
+      get '/merchants/find_all', to: "merchants#index"
+      get '/merchants/find', to: "merchants#show"
 
-      get '/items/find_all', to: "items#index", defaults: { format: :json }
-      get '/items/find', to: "items#show", defaults: { format: :json }
-      get '/items/random', to: "random_item#show", defaults: { format: :json }
-      resources :items, only: [:index, :show], defaults: { format: :json }
+      resources :merchants, only: [:index, :show] do
+        get '/items', to: "merchants/items#index"
+        get '/invoices', to: "merchants/invoices#index"
+      end
 
-      get '/customers/find_all', to: "customers#index", defaults: { format: :json }
-      get '/customers/find', to: "customers#show", defaults: { format: :json }
-      get '/customers/random', to: "random_customer#show", defaults: { format: :json }
-      resources :customers, only: [:index, :show], defaults: { format: :json }
+      get '/items/find_all', to: "items#index"
+      get '/items/find', to: "items#show"
+      get '/items/random', to: "random_item#show"
+      resources :items, only: [:index, :show]
 
-      get '/invoices/find_all', to: "invoices#index", defaults: { format: :json }
-      get '/invoices/find', to: "invoices#show", defaults: { format: :json }
-      get '/invoices/random', to: "random_invoice#show", defaults: { format: :json }
-      resources :invoices, only: [:index, :show], defaults: { format: :json }
+      get '/customers/find_all', to: "customers#index"
+      get '/customers/find', to: "customers#show"
+      get '/customers/random', to: "random_customer#show"
+      resources :customers, only: [:index, :show]
 
-      get '/invoice_items/find_all', to: "invoice_items#index", defaults: { format: :json }
-      get '/invoice_items/find', to: "invoice_items#show", defaults: { format: :json }
-      get '/invoice_items/random', to: "random_invoice_item#show", defaults: { format: :json }
-      resources :invoice_items, only: [:index, :show], defaults: { format: :json }
+      get '/invoices/find_all', to: "invoices#index"
+      get '/invoices/find', to: "invoices#show"
+      get '/invoices/random', to: "random_invoice#show"
+      resources :invoices, only: [:index, :show]
 
-      get '/transactions/find_all', to: "transactions#index", defaults: { format: :json }
-      get '/transactions/find', to: "transactions#show", defaults: { format: :json }
-      get '/transactions/random', to: "random_transaction#show", defaults: { format: :json }
-      resources :transactions, only: [:index, :show], defaults: { format: :json }
+      get '/invoice_items/find_all', to: "invoice_items#index"
+      get '/invoice_items/find', to: "invoice_items#show"
+      get '/invoice_items/random', to: "random_invoice_item#show"
+      resources :invoice_items, only: [:index, :show]
+
+      get '/transactions/find_all', to: "transactions#index"
+      get '/transactions/find', to: "transactions#show"
+      get '/transactions/random', to: "random_transaction#show"
+      resources :transactions, only: [:index, :show]
     end
   end
 end
