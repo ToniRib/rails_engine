ccaeed79d20fd260dd593bcd36a7350e5a2d2eaf
ToniRib <tonimarierib@gmail.com>
2016-02-24 16:06:46 -0700

    Fix test for merchants by revenue controller

--- spec/controllers/merchants/top_merchants_by_revenue_controller_spec.rb
+++ spec/controllers/merchants/top_merchants_by_revenue_controller_spec.rb
@@ -11,11 +11,13 @@ RSpec.describe Api::V1::Merchants::TopMerchantsByRevenueController, type: :contr
       expect(response).to have_http_status(200)
     end
 
-    xit "returns all of the merchant's items in JSON format" do
-      set_up_merchants_with_transactions
+    it "returns all of the merchant's items in JSON format" do
+      merchant1, merchant2, merchant3 = load_merchants_with_transactions
 
-      get :index, quantity: 3, format: :json
+      get :index, quantity: 2, format: :json
 
+      expect(json_response.first["id"]).to eq(merchant3.id)
+      expect(json_response.last["id"]).to eq(merchant2.id)
     end
   end
 end
