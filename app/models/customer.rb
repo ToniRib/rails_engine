6d7f84e334edec0d60bd4d1808226df30753449d
ToniRib <tonimarierib@gmail.com>
2016-02-24 19:42:10 -0700

    Add functionality for customer's favorite merchant - all spec harness tests passing!!

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,18 @@
 class Customer < ActiveRecord::Base
   has_many :invoices
   has_many :transactions, through: :invoices
+  has_many :merchants, through: :invoices
 
   default_scope { order(id: :asc) }
+
+  def favorite_merchant
+    merchant = merchants
+                 .select('id', 'name', 'COUNT(transactions.id) AS number_of_transactions' )
+                 .joins(:invoices => [:merchant, :transactions])
+                 .where("result='success'")
+                 .group(:id)
+                 .reorder('number_of_transactions DESC')
+                 .take(1).first
+    { "id" => merchant.id, "name" => merchant.name }
+  end
 end
